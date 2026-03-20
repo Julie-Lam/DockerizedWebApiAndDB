ff67fa37af7307a289febeab04b84ea46c810d7b
Julie <lam.julie@hotmail.com>
2026-03-20 22:18:43 +1100

    Add a db snapshot

--- DockerizedWebApiAndDB/Models/Contact.cs
+++ DockerizedWebApiAndDB/Models/Contact.cs
@@ -2,7 +2,7 @@
 {
     public class Contact
     {
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
